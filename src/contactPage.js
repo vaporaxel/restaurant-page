f3bf33ab2ec730a9228bc285c81e02f323c31bc3
Axel <axelarep@gmail.com>
2022-04-13 22:23:17 -0500

    The page is complete, in this case with basic content elements

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,7 +1,34 @@
 function contactPageLoad(){
+
     // Remove other pages (elements of their functions)
-    // let oldHeader = document.querySelector('.header')
-    // let oldItemCard = document.querySelector('.item-card')
+    let oldHeader = document.querySelector('#content')
+    let oldItemCard = document.querySelector('.item-card')
+
+    oldHeader.innerHTML = ''
+    oldHeader.innerText = ''
+    // oldItemCard.innerHTML = ''
+    // oldItemCard.classList.remove('item-card')
+    
+    // Select content container
+    let content = document.getElementById('content')
+
+    // Create elements
+    let header = document.createElement('div')
+    let mainTitle = document.createElement('h1')
+    let headerText = document.createElement('p')
+
+    // Inner content of elements created
+    mainTitle.innerText = 'Contact Us!'
+    headerText.innerText = 'Here will be the contact information'
+
+    // Add classes to specific created elements
+    header.className = 'header'
+
+     // Append created elements
+     content.appendChild(header)
+     header.appendChild(mainTitle)
+     header.appendChild(headerText)
+
 }
 
 export  {contactPageLoad};
